dd16c771ff6bd9eff0dba052ac47cde2875f6d1f
Adam Greenthal <adamgreenthal@gmail.com>
2020-05-26 13:18:17 -0700

    commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Main from '../Main/Main';
-import Schedule from '../Schedule/Schedule';
 import Footer from '../Footer/Footer';
-import Twitter from '../Main/Twitter';
-import News from '../Main/News';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Row, Col } from 'react-bootstrap';
 import NavBar1 from '../NavBar/NavBar1';
@@ -54,7 +51,6 @@ function Home() {
                 </Col>
                 <Col>
                     <Row>
-                        {/* <Twitter /> */}
                         {/* <News /> */}
                     </Row>
                 </Col>
--- src/Components/Roster/RosterPage.js
+++ src/Components/Roster/RosterPage.js
@@ -2,19 +2,24 @@ import React from 'react';
 import Roster from '../Roster/Roster';
 import RosterTable from '../Roster/RosterTable'
 import Footer from '../Footer/Footer';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { Container, Row } from 'react-bootstrap';
 import NavBar1 from '../NavBar/NavBar1';
 // import NavBar from '../NavBar/NavBar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const useStyles = makeStyles((theme) => ({
+    main: {
+        backgroundColor: '#F7FAF8'
+    }
+}));
 
 function RosterPage() {
-    // const classes = useStyles();
+    const classes = useStyles();
     return (
         <>
-            <Container fluid>
+            <Container className={classes.main} fluid>
                 <Row>
                     <NavBar1 />
                 </Row>
